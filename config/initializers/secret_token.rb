3484ea2efd5f29080ada3e86519a60cd765359ac
Stevan Litobac <stevan@litobac.com>
2014-03-03 13:30:06 +0000

    [INIT] Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reffinder::Application.config.secret_key_base = '9967cfecf5b0d507ee6e7ecc53407e6743e76b0505f07fd99deafd97762552fe426cc4afcf5bd0084dbf5168b48bffa4862a6c816377b35053e2522b95e7ac26'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Reffinder::Application.config.session_store :cookie_store, key: '_reffinder_session'
